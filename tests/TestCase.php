e239451d41df0853a030c2363221d33a1cf7a0bd
xirwan <bokutou20@gmail.com>
2025-09-22 01:09:18 +0700

    Menambahkan tests file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Disable ALL middleware untuk testing
+        $this->withoutMiddleware();
+    }
 }
